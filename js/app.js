3df41af7b5e8a41d55dbc58101bcc6dcb7ad8d4c
Christian John Gonzalez <bournecreative@gmail.com>
2021-03-26 23:46:24 -0700

    incorporate countdown and tabs in demo

--- js/app.js
+++ js/app.js
@@ -165,9 +165,70 @@
         }
     }
 
+    //
+    const countdownTimer = {
+        init: () => {
+            this.timers = Array.prototype.slice.call(document.querySelectorAll('.countdown__container'));
+
+            if (!this.timers) {
+                return;
+            }
+
+            this.timers.forEach(timer => {
+                this.time = timer;
+                countdownTimer.count(this.time);
+            })
+        },
+        count: (root) => {
+            let pendulum;
+            let targetDate;
+            const eleDate = root.getAttribute('data-countdown');
+
+            if (eleDate === '') {
+                const d = new Date();
+                targetDate = d.setDate(d.getDate() + 1);
+            } else {
+                targetDate = new Date(eleDate);
+            }
+
+            pendulum = setInterval(() => {
+                const current = Date.now();
+                const diff = targetDate - current;
+                if (diff < 0) {
+                    clearTimeout(pendulum);
+                }
+                countdownTimer.logTime(diff, root);
+            }, 1000);
+        },
+        logTime: (diff, root) => {
+            const dayDisplay = root.querySelector('.countdown__days');
+            const hourDisplay = root.querySelector('.countdown__hours');
+            const minsDisplay = root.querySelector('.countdown__mins');
+            const secsDisplay = root.querySelector('.countdown__secs');
+
+            const days = Math.floor(diff / 1000 / 60 / 60 / 24);
+            const hours = Math.floor(diff / 1000 / 60 / 60) % 24;
+            const mins = Math.floor(diff / 1000 / 60) % 60;
+            const secs = Math.floor(diff / 1000) % 60;
+
+            dayDisplay.textContent = `${days < 10 ? 0 : ''}${days} :`;
+            hourDisplay.textContent = `${hours < 10 ? 0 : ''}${hours} :`;
+            minsDisplay.textContent = `${mins < 10 ? 0 : ''}${mins} :`;
+            secsDisplay.textContent = `${secs < 10 ? 0 : ''}${secs}`;
+            if (diff < 0) {
+                dayDisplay.textContent = `00 :`;
+                hourDisplay.textContent = `00 :`;
+                minsDisplay.textContent = `00 :`;
+                secsDisplay.textContent = `00`;
+            }
+        }
+    }
+
+
     window.addEventListener('load', () => {
         dropDown.init();
         accordion.init();
+        countdownTimer.init();
     })
 
 
