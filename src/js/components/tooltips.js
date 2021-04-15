20ebbe7b4fa38064da33968044cc63069469a70f
Christian John Gonzalez <bournecreative@gmail.com>
2021-04-14 23:36:49 -0700

    Incoporate PopperJs into tooltips for positioning. Basic functionality in place to ensure tooltips are displayed based on available space.

--- src/js/components/tooltips.js
+++ src/js/components/tooltips.js
@@ -1,55 +1,77 @@
+import { createPopper } from '@popperjs/core';
+
 const tooltips = {
     init() {
         this.tooltipElements = Array.prototype.slice.call(document.querySelectorAll('.tooltip'));
         if (!this.tooltipElements.length) {
             return;
         }
-        console.log(this.tooltipElements)
         this.registerEvents();
         this.renderToolTipContainer();
     },
     registerEvents() {
-        this.toolTipTrigger = document.querySelector('.tooltip');
-        this.toolTipTrigger.addEventListener('click', (e) => {
-            if (e.target.className === 'tooltip__button') {
-                this.openTip(this.toolTipTrigger);
-            }
+        this.tooltipElements.forEach(tip => {
+            tip.addEventListener('click', (e) => {
+                if (e.target.className === 'tooltip__button') {
+                    this.openTip(tip);
+                }
+            })
+            tip.addEventListener('mouseenter', (e) => {
+                if (e.target.className === 'tooltip__button') {
+                    this.openTip(tip);
+                }
+            }, true)
+            tip.addEventListener('mouseleave', (e) => {
+                if (e.target.className === 'tooltip__button') {
+                    this.closeTip();
+                }
+            }, true)
         })
-        this.toolTipTrigger.addEventListener('mouseenter', (e) => {
-            if (e.target.className === 'tooltip__button') {
-                this.openTip(this.toolTipTrigger);
-            }
-        }, true)
-        this.toolTipTrigger.addEventListener('mouseleave', (e) => {
-            if (e.target.className === 'tooltip__button') {
-                this.closeTip();
-            }
-        }, true)
     },
     renderToolTipContainer() {
-        const toolTipHTML = `<div class="tooltip__container" aria-hidden="true" role="none"></div>`;
+        const toolTipHTML = `<div id="tooltipContainer" class="tooltip__container" aria-hidden="true" role="tooltip"><div class="arrow" data-popper-arrow></div><div class="content"></div></div>`;
         document.body.insertAdjacentHTML('beforeend', toolTipHTML);
         this.toolTipContainer = document.querySelector('.tooltip__container');
     },
     openTip(tooltip) {
         const toolTipText = tooltip.querySelector('.tooltip__wrapper').getAttribute('data-tooltip-content');
-        const tipCoords = tooltip.getBoundingClientRect();
+        // const tipCoords = tooltip.getBoundingClientRect();
+        const arrow = this.toolTipContainer.querySelector('.arrow');
 
         if (!this.toolTipContainer.classList.contains('active')) {
-            const coords = {
-                top: tipCoords.top + window.scrollY,
-                left: tipCoords.left + window.scrollX
-            }
-            this.toolTipContainer.textContent = toolTipText;
-            const offsetLeft = (this.toolTipContainer.offsetWidth - tooltip.offsetWidth) / 2;
-            const offsetTop = tooltip.offsetWidth
-            this.toolTipContainer.style.transform = `translate(${coords.left - offsetLeft}px, ${coords.top + 32}px)`
+            // const coords = {
+            //     top: tipCoords.top + window.scrollY,
+            //     left: tipCoords.left + window.scrollX
+            // }
+
+            createPopper(tooltip, this.toolTipContainer, {
+                modifiers: [
+
+                    {
+                        name: 'offset',
+                        options: {
+                            offset: [0, 16],
+
+                        },
+                    },
+                    {
+                        name: 'arrow',
+                        options: {
+                            element: arrow,
+                        },
+                    },
+                ],
+            });
+            this.toolTipContainer.querySelector('.content').textContent = toolTipText;
+            // const offsetLeft = (this.toolTipContainer.offsetWidth - tooltip.offsetWidth) / 2;
+            // const offsetTop = tooltip.offsetWidth
+            // this.toolTipContainer.style.transform = `translate(${coords.left - offsetLeft}px, ${coords.top + 32}px)`
             this.toolTipContainer.classList.toggle('active');
         }
     },
     closeTip() {
         this.toolTipContainer.classList.remove('active');
-        this.toolTipContainer.textContent = '';
+        this.toolTipContainer.querySelector('.content').textContent = '';
     }
 }
 
