bb38522a35c36e066e56077257d3828d9e10d738
Christian John Gonzalez <bournecreative@gmail.com>
2021-05-05 20:52:49 -0700

    Updated swiper to remove/add navigation arrows based on breakpoint using window.matchMedia. Also refactored carousel.js to use the Module Reveal design pattern

--- src/js/components/carousel.js
+++ src/js/components/carousel.js
@@ -1,18 +1,28 @@
-import Swiper from 'swiper/bundle';
+import SwiperCore, { Navigation, Pagination } from 'swiper/core'
+SwiperCore.use([Navigation, Pagination]);
 
-const carousel = {
-    init() {
+const carousel = function () {
+
+    const init = function () {
         this.carousel = Array.prototype.slice.call(document.querySelectorAll('.swiper-container'));
         if (!this.carousel.length) {
             return;
         }
         this.enableCarousel()
-    },
-    enableCarousel() {
-        this.mySwiper = new Swiper('.swiper-container', {
-            slidesPerView: 1,
+    }
+
+    const enableCarousel = function () {
+        this.mySwiper = new SwiperCore('.swiper-container', {
             spaceBetween: 0,
             loop: true,
+            breakpoints: {
+                320: {
+                    slidesPerView: 1
+                },
+                768: {
+                    slidesPerView: 1
+                }
+            },
             pagination: {
                 el: '.swiper-pagination',
                 clickable: true,
@@ -20,9 +30,39 @@ const carousel = {
             navigation: {
                 nextEl: '.swiper-button-next',
                 prevEl: '.swiper-button-prev',
+            },
+            on: {
+                breakpoint: function (swiper) {
+                    if (swiper.navigation.prevEl !== undefined || swiper.navigation.nextEl !== undefined) {
+                        setNavigation(swiper)
+                    }
+                }
             }
         })
     }
+
+    const setNavigation = function (swiper) {
+        const mdBreakPoint = window.matchMedia('(min-width: 768px)').matches;
+        if (mdBreakPoint === true) {
+            swiper.navigation.prevEl.classList.remove('swiper-button-hidden')
+            swiper.navigation.nextEl.classList.remove('swiper-button-hidden')
+            swiper.navigation.prevEl.classList.add('swiper-button-prev')
+            swiper.navigation.nextEl.classList.add('swiper-button-next')
+            swiper.navigation.update();
+        } else {
+            swiper.navigation.prevEl.classList.remove('swiper-button-prev')
+            swiper.navigation.nextEl.classList.remove('swiper-button-next')
+            swiper.navigation.prevEl.classList.add('swiper-button-hidden')
+            swiper.navigation.nextEl.classList.add('swiper-button-hidden')
+            swiper.navigation.update();
+        }
+    }
+
+    return {
+        init: init,
+        enableCarousel: enableCarousel,
+        setNavigation: setNavigation
+    }
 }
 
-export default carousel;
+export default carousel();
