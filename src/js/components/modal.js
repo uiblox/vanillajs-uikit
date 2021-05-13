0bbf3c3ae2fe0485f79ecae674aebdeefc92e30e
Christian John Gonzalez <bournecreative@gmail.com>
2021-05-12 21:19:32 -0700

    refactor modal trigger to use an eventHandler function. Add logic to position modal

--- src/js/components/modal.js
+++ src/js/components/modal.js
@@ -1,4 +1,3 @@
-
 const Modal = {
     init() {
         this.modal = document.querySelectorAll('.modal__trigger')
@@ -11,15 +10,24 @@ const Modal = {
     },
     registerEventListener() {
         Array.from(this.modal).forEach(ele => {
-            const target = ele.getAttribute('data-modal-trigger')
-            const modal = document.querySelector(`[data-modal=${target}]`)
-            ele.addEventListener('click', () => {
-                this.toggleModal(modal)
-            })
+            this.eventHandler(ele)
         })
     },
-    toggleModal(modal) {
+    eventHandler(ele) {
+        ele.addEventListener('click', this.toggleModal)
+    },
+    toggleModal() {
+        const target = this.getAttribute('data-modal-trigger');
+        const modal = document.querySelector(`[data-modal=${target}]`);
+        const body = document.querySelector('body');
+
         modal.classList.toggle('modal__state-hidden')
+
+        if (!modal.classList.contains('modal__state-hidden')) {
+            body.classList.add('no-scroll')
+        } else {
+            body.classList.remove('no-scroll')
+        }
     }
 }
 
