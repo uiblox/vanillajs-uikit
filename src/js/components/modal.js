efdee5d3b49e62ccaba33049b1e0004c50542628
Christian John Gonzalez <bournecreative@gmail.com>
2021-05-13 22:30:45 -0700

    refactor modal.js to leverage toogle modal functionality to both open and close modal. Update needed data attributes in html. Update styles for modal.

--- src/js/components/modal.js
+++ src/js/components/modal.js
@@ -1,33 +1,49 @@
 const Modal = {
     init() {
-        this.modal = document.querySelectorAll('.modal__trigger')
+        this.buttons = document.querySelectorAll('.button');
 
-        if (!this.modal.length) {
+        if (!this.buttons.length) {
             return;
         }
 
         this.registerEventListener();
     },
     registerEventListener() {
-        Array.from(this.modal).forEach(ele => {
-            this.eventHandler(ele)
+        Array.from(this.buttons).forEach(ele => {
+            if (ele.classList.contains("button__modal-trigger")) {
+                this.setHandler(ele);
+            }
+            if (ele.classList.contains("button__modal-close")) {
+                this.closeHandler(ele);
+            }
         })
     },
-    eventHandler(ele) {
-        ele.addEventListener('click', this.toggleModal)
+    setHandler(ele) {
+        ele.addEventListener('click', this.setModal);
     },
-    toggleModal() {
+    closeHandler(ele) {
+        ele.addEventListener('click', this.closeModal);
+    },
+    setModal() {
         const target = this.getAttribute('data-modal-trigger');
         const modal = document.querySelector(`[data-modal=${target}]`);
+        Modal.toggleModal(modal)
+    },
+    toggleModal(modal) {
         const body = document.querySelector('body');
 
-        modal.classList.toggle('modal__state-hidden')
+        modal.classList.toggle('modal__state-hidden');
 
         if (!modal.classList.contains('modal__state-hidden')) {
             body.classList.add('no-scroll')
         } else {
             body.classList.remove('no-scroll')
         }
+    },
+    closeModal() {
+        const target = this.getAttribute('data-close-modal');
+        const modal = document.querySelector(`[data-modal=${target}]`);
+        Modal.toggleModal(modal)
     }
 }
 
