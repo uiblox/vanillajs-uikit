e4a715eb50b58a1479ef58a83867d0badc5567a4
Christian John Gonzalez <bournecreative@gmail.com>
2021-05-14 22:37:20 -0700

    implemented keyboard trap, added outline for focused elements related to modal and keyboard trap

--- src/js/components/modal.js
+++ src/js/components/modal.js
@@ -1,14 +1,17 @@
 const Modal = {
     init() {
         this.buttons = document.querySelectorAll('.button');
-
+        this.modals = document.querySelectorAll('.modal');
         if (!this.buttons.length) {
             return;
         }
-
-        this.registerEventListener();
+        if (!this.modals.length) {
+            return;
+        }
+        this.registerModalListeners();
+        this.registerModalTraps();
     },
-    registerEventListener() {
+    registerModalListeners() {
         Array.from(this.buttons).forEach(ele => {
             if (ele.classList.contains("button__modal-trigger")) {
                 this.setHandler(ele);
@@ -16,6 +19,11 @@ const Modal = {
             if (ele.classList.contains("button__modal-close")) {
                 this.closeHandler(ele);
             }
+        })
+    },
+    registerModalTraps() {
+        Array.from(this.modals).forEach(modal => {
+            this.keyBoardTrap(modal);
         })
     },
     setHandler(ele) {
@@ -27,7 +35,7 @@ const Modal = {
     setModal() {
         const target = this.getAttribute('data-modal-trigger');
         const modal = document.querySelector(`[data-modal=${target}]`);
-        Modal.toggleModal(modal)
+        Modal.toggleModal(modal);
     },
     toggleModal(modal) {
         const body = document.querySelector('body');
@@ -35,15 +43,47 @@ const Modal = {
         modal.classList.toggle('modal__state-hidden');
 
         if (!modal.classList.contains('modal__state-hidden')) {
-            body.classList.add('no-scroll')
+            body.classList.add('no-scroll');
         } else {
-            body.classList.remove('no-scroll')
+            body.classList.remove('no-scroll');
         }
     },
     closeModal() {
         const target = this.getAttribute('data-close-modal');
         const modal = document.querySelector(`[data-modal=${target}]`);
-        Modal.toggleModal(modal)
+        const modalTrigger = document.querySelector(`[data-modal-trigger=${target}]`);
+        modalTrigger.focus();
+        Modal.toggleModal(modal);
+
+    },
+    keyBoardTrap(ele) {
+        ele.addEventListener('keydown', modalTrap);
+        const focusableChildren = ele.querySelectorAll('a, button');
+        const first = focusableChildren[0];
+        const last = focusableChildren[focusableChildren.length - 1];
+
+        function modalTrap(e) {
+            console.log(focusableChildren)
+            if (e.keyCode === 9) {
+                if (e.shiftKey) {
+                    //if first element is focused, go to the last element
+                    if (document.activeElement === first) {
+                        e.preventDefault();
+                        last.focus()
+                    }
+                } else {
+                    //if last element is focused, go to the first element
+                    if (document.activeElement === last) {
+                        e.preventDefault();
+                        first.focus();
+                    }
+                }
+            }
+            // escape key closes modal
+            if (e.keyCode === 27) {
+                closeModal();
+            }
+        }
     }
 }
 
