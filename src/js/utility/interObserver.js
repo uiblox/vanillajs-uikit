b9e9f05767148df2b3dc3350c664aae4cd3f9568
Christian John Gonzalez <bournecreative@gmail.com>
2021-05-19 23:38:00 -0700

    create intersectional utility

---
+++ src/js/utility/interObserver.js
@@ -0,0 +1,17 @@
+const interObserver = {
+
+    init(target, handleIntersect) {
+        let observer;
+
+        let options = {
+            root: null, /* not setting this property or using null sets root to viewport */
+            rootMargin: "0px", /* negitive will set a inner distance*/
+            threshold: 1 /* 0 to 1 scale - 1 means the element has to be 100% in the viewport*/
+        };
+
+        observer = new IntersectionObserver(handleIntersect, options);
+        observer.observe(target);
+    }
+}
+
+export default interObserver;
