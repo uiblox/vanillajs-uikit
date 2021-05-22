22a9db8242f6acb6eb5c1c4880302c50237c27b8
Christian John Gonzalez <bournecreative@gmail.com>
2021-05-22 00:29:58 -0700

    add svg icon, update title for video autoplay, add logic to hide reveal video play button based on video play state

--- src/js/components/autoPlay.js
+++ src/js/components/autoPlay.js
@@ -4,22 +4,26 @@ const autoPlay = {
     init() {
         this.videos = document.querySelectorAll('.video');
         if (!this.videos) {
-            return
+            return;
         }
         this.setObserver();
     },
     setObserver() {
         this.videos.forEach(video => {
-            interObserver(video, this.setPlay)
+            interObserver(video, this.setPlay);
         })
     },
     setPlay(vid) {
-        const video = vid[0]
+        const video = vid[0];
+        const videoContainer = video.target.parentNode;
+        const videoBtn = videoContainer.querySelector('.video__icon');
         if (video.isIntersecting) {
             video.target.currentTime = 0;
             video.target.play();
+            videoBtn.style.visibility = "hidden";
         } else {
             video.target.pause();
+            videoBtn.style.visibility = "visible";
         }
     }
 }
